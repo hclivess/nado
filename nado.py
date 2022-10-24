747682a0fb91e85fd5a24736aa99c7e4c7706f27
hclivess <admin@bismuth.cz>
2022-10-24 18:45:38 +0200

    genesis update

--- nado.py
+++ nado.py
@@ -410,10 +410,10 @@ if __name__ == "__main__":
         make_folders()
         make_genesis(
             address="ndo18c3afa286439e7ebcb284710dbd4ae42bdaf21b80137b",
-            balance=1000000000000000,
-            ip="89.176.130.244",
+            balance=10000000000000000,
+            ip="78.102.98.72",
             port=9173,
-            timestamp=1657829259,
+            timestamp=1666629900,
             logger=logger,
         )
 
