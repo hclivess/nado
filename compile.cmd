43f6beb79b2381cbff9e9de87fb1d533a4c35e67
hclivess <admin@bismuth.cz>
2022-11-26 19:55:58 +0100

    compilation updates

--- compile.cmd
+++ compile.cmd
@@ -1,2 +1,5 @@
 python -m nuitka nado.py --standalone --windows-icon-from-ico=graphics\icon.ico
+xcopy /e /i /y templates nado.dist\templates
+xcopy /e /i /y static nado.dist\static
+xcopy /e /i /y graphics nado.dist\graphics
 pause
