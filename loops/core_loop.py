a6137204405d687b26d43631dbb59736aa769028
hclivess <admin@bismuth.cz>
2022-12-07 12:11:19 +0100

    announce win on amount

--- loops/core_loop.py
+++ loops/core_loop.py
@@ -369,7 +369,7 @@ class CoreClient(threading.Thread):
 
                 gen_elapsed = get_timestamp_seconds() - gen_start
 
-                if self.memserver.ip == block['block_ip'] and self.memserver.address == block['block_creator']:
+                if self.memserver.ip == block['block_ip'] and self.memserver.address == block['block_creator'] and block['block_reward'] > 0:
                     self.logger.warning(f"$$$ Congratulations! You won! $$$")
 
                 self.logger.warning(f"Block hash: {block['block_hash']}")
