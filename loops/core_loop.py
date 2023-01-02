a71ef0fea147c6626a9625f87a185fe8fb9fc885
hclivess <admin@bismuth.cz>
2023-01-02 14:22:00 +0100

    compatibility wip

--- loops/core_loop.py
+++ loops/core_loop.py
@@ -406,7 +406,7 @@ class CoreClient(threading.Thread):
                 gen_start = get_timestamp_seconds()
                 self.logger.warning(f"Producing block")
 
-                if remote:
+                if remote and self.memserver.latest_block["block_number"] > 19000:
                     block = self.restructure_remote_block(block)
 
                 self.validate_transactions_in_block(block=block,
