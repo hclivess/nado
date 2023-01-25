3fc256955b4d8c7552fa838a0fb5e7cd852c8b04
hclivess <admin@bismuth.cz>
2023-01-25 20:50:50 +0100

    quickfix

--- loops/consensus_loop.py
+++ loops/consensus_loop.py
@@ -166,6 +166,7 @@ class ConsensusClient(threading.Thread):
 
 def change_trust(trust_pool, peer, value):
     """a queue would be ideal, but operation not critical, skips don't matter"""
-    if peer in trust_pool.copy().keys():
+    trust_pool_copy = trust_pool.copy()
+    if peer in trust_pool_copy.keys():
         trust_pool[peer] += value
-    return trust_pool
+    return trust_pool_copy
