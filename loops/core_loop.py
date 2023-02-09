123c849c3902291240257aeaa14d3aedd26dfce4
hclivess <admin@bismuth.cz>
2023-02-09 11:46:27 +0100

    experimental: purge peers that do not quality

--- loops/core_loop.py
+++ loops/core_loop.py
@@ -246,7 +246,7 @@ class CoreClient(threading.Thread):
                             else:
                                 self.logger.info(f"{peer} not qualified for sync: {qualifies['flag']}")
 
-                                if peer not in self.memserver.purge_peers_list:
+                                if peer not in self.memserver.purge_peers_list and peer not in self.memserver.unreachable:
                                     self.memserver.purge_peers_list.append(peer)
                 else:
                     self.logger.info(f"Ran out of options when picking trusted hash")
