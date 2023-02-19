62290f15d3043962ee297bd942daa4d5370575cf
hclivess <admin@bismuth.cz>
2023-02-19 18:46:20 +0100

    update archiver to tarball because that's easier to work with, especially in linux

--- archiver.py
+++ archiver.py
@@ -1,17 +1,20 @@
-import py7zr
-
+import tarfile
 from ops.data_ops import get_home
 
 
 def make_archive(output_filename, source_dirs):
-    with py7zr.SevenZipFile(output_filename, 'w') as z:
-        for dir in source_dirs:
-            print(f"processing {dir}")
-            z.writeall(dir, arcname="nado")
+    with tarfile.open(f"{output_filename}.tar.gz", "w:gz") as tar:
+        for file in source_dirs:
+            try:
+                print("Compressing", file)
+                tar.add(file, arcname=file)
+            except Exception as e:
+                print(f"Error compressing {file}: {e}")
 
+        print("Compression finished for", source_dirs)
 
 if __name__ == "__main__":
     source_dirs = [f"{get_home()}/blocks", f"{get_home()}/index", f"{get_home()}/peers"]
-    output_filename = "nado_archive.7z"
+    output_filename = "nado_archive"
 
     make_archive(output_filename, source_dirs)
