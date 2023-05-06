6c397e69da464c97d9028c70860b3d6c8902c474
hclivess <admin@bismuth.cz>
2023-05-06 16:04:51 +0200

    fix archiver.py

--- archiver.py
+++ archiver.py
@@ -1,20 +1,28 @@
 import tarfile
 from ops.data_ops import get_home
+from pathlib import Path
 
 
 def make_archive(output_filename, source_dirs):
     with tarfile.open(f"{output_filename}.tar.gz", "w:gz") as tar:
-        for file in source_dirs:
+        for path in source_dirs:
             try:
-                print("Compressing", file)
-                tar.add(file, arcname=file)
+                print("Compressing", path["files"])
+                tar.add(path["files"], arcname=path["dir"])
             except Exception as e:
-                print(f"Error compressing {file}: {e}")
+                print(f"Error compressing {path}: {e}")
 
         print("Compression finished for", source_dirs)
 
+
 if __name__ == "__main__":
-    source_dirs = [f"{get_home()}/blocks", f"{get_home()}/index", f"{get_home()}/peers"]
+    source_dirs = [
+        {"files": f"{get_home()}/peers", "dir": "peers"},
+        {"files": f"{get_home()}/blocks", "dir": "blocks"},
+        {"files": f"{get_home()}/index", "dir": "index"},
+        {"files": f"{get_home()}/peers", "dir": "peers"}
+    ]
+
     output_filename = "nado_archive"
 
     make_archive(output_filename, source_dirs)
