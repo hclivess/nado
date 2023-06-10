b71df335cdf13c6dc6aceb6ec4da1fc221e43807
hclivess <admin@bismuth.cz>
2023-06-10 15:49:49 +0200

    remove so people dont run it twice

--- convert_ends.py
+++ convert_ends.py
@@ -1,5 +1,6 @@
 from ops.data_ops import get_home
 import json
+import os
 
 genesis = "6514c2b2fac0d1e820c1d24dbcf36dd34532b59ed4c268b15c341663ce505b9f"
 
@@ -11,3 +12,5 @@ new_dict = {"earliest_block": genesis,
 
 with open(f"{get_home()}/index/block_ends.dat", "w") as new:
     json.dump(new_dict, new)
+
+os.remove(f"{get_home()}/index/latest_block.dat")
