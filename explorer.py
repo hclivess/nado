db4efbdca06df2b9baef614799ec60d2125e6f78
hclivess <admin@bismuth.cz>
2023-07-19 14:24:40 +0200

    block number handler

--- explorer.py
+++ explorer.py
@@ -20,6 +20,19 @@ class HomeHandler(tornado.web.RequestHandler):
     def get(self):
         self.home()
 
+class BlockNumberHandler(tornado.web.RequestHandler):
+    def block(self, block):
+        data = self.get_data(block)
+        self.render("templates/explorer.html",
+                    data=data)
+
+    def get_data(self, block):
+        data = requests.get(f"{nado_node}/get_block_number?number={block}").text
+        return json.loads(data)
+
+    def get(self, parameters):
+        entry = AccountHandler.get_argument(self, "entry")
+        self.block(block=entry)
 
 class AccountHandler(tornado.web.RequestHandler):
     def account(self, account):
@@ -35,6 +48,7 @@ class AccountHandler(tornado.web.RequestHandler):
         entry = AccountHandler.get_argument(self, "entry")
         self.account(account=entry)
 
+
 class TransactionHandler(tornado.web.RequestHandler):
     def transaction(self, txid):
         data = self.get_data(txid)
@@ -49,12 +63,29 @@ class TransactionHandler(tornado.web.RequestHandler):
         entry = TransactionHandler.get_argument(self, "entry")
         self.transaction(txid=entry)
 
+
+class SupplyHandler(tornado.web.RequestHandler):
+    def supply(self):
+        data = self.get_data()
+        self.render("templates/supply.html",
+                    data=data)
+
+    def get_data(self):
+        data = requests.get(f"{nado_node}/get_supply?&readable=true").text
+        return json.loads(data)
+
+    def get(self):
+        self.supply()
+
+
 async def make_app(port):
     application = tornado.web.Application(
         [
             (r"/", HomeHandler),
             (r"/get_account(.*)", AccountHandler),
             (r"/get_transaction(.*)", TransactionHandler),
+            (r"/get_block_number(.*)", BlockNumberHandler),
+            (r"/get_supply", SupplyHandler),
             (r"/static/(.*)", tornado.web.StaticFileHandler, {"path": "static"}),
             (r'/(favicon.ico)', tornado.web.StaticFileHandler, {"path": "graphics"}),
 
