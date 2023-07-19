6a8ce9d416e3f7969a4ad41a52b55e22b23103fd
hclivess <admin@bismuth.cz>
2023-07-19 13:34:28 +0200

    account handler

--- explorer.py
+++ explorer.py
@@ -20,11 +20,27 @@ class HomeHandler(tornado.web.RequestHandler):
     def get(self):
         self.home()
 
+class AccountHandler(tornado.web.RequestHandler):
+    def account(self, account):
+        data = self.get_data(account)
+        print(data)
+        self.render("templates/account.html",
+                    data=data)
+
+    def get_data(self, account):
+        data = requests.get(f"{nado_node}/get_account?address={account}&readable=true").text
+        return json.loads(data)
+
+    def get(self, parameters):
+        entry = AccountHandler.get_argument(self, "entry")
+        print(entry)
+        self.account(account=entry)
 
 async def make_app(port):
     application = tornado.web.Application(
         [
             (r"/", HomeHandler),
+            (r"/get_account(.*)", AccountHandler),
             (r"/static/(.*)", tornado.web.StaticFileHandler, {"path": "static"}),
             (r'/(favicon.ico)', tornado.web.StaticFileHandler, {"path": "graphics"}),
 
