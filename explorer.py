88e5be89dfd251d69ef13fb92af0259f809cf576
hclivess <admin@bismuth.cz>
2023-07-19 13:56:03 +0200

    txid handler

--- explorer.py
+++ explorer.py
@@ -20,10 +20,10 @@ class HomeHandler(tornado.web.RequestHandler):
     def get(self):
         self.home()
 
+
 class AccountHandler(tornado.web.RequestHandler):
     def account(self, account):
         data = self.get_data(account)
-        print(data)
         self.render("templates/account.html",
                     data=data)
 
@@ -33,14 +33,28 @@ class AccountHandler(tornado.web.RequestHandler):
 
     def get(self, parameters):
         entry = AccountHandler.get_argument(self, "entry")
-        print(entry)
         self.account(account=entry)
+
+class TransactionHandler(tornado.web.RequestHandler):
+    def transaction(self, txid):
+        data = self.get_data(txid)
+        self.render("templates/transaction.html",
+                    data=data)
+
+    def get_data(self, txid):
+        data = requests.get(f"{nado_node}/get_transaction?txid={txid}&readable=true").text
+        return json.loads(data)
+
+    def get(self, parameters):
+        entry = TransactionHandler.get_argument(self, "entry")
+        self.transaction(txid=entry)
 
 async def make_app(port):
     application = tornado.web.Application(
         [
             (r"/", HomeHandler),
             (r"/get_account(.*)", AccountHandler),
+            (r"/get_transaction(.*)", TransactionHandler),
             (r"/static/(.*)", tornado.web.StaticFileHandler, {"path": "static"}),
             (r'/(favicon.ico)', tornado.web.StaticFileHandler, {"path": "graphics"}),
 
