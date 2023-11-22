2c7ca1ed975bf07aa57219564bdc7894d4bd8b87
hclivess <admin@bismuth.cz>
2023-11-22 22:06:02 +0100

    upgrade backup

--- backup.py
+++ backup.py
@@ -1,7 +1,6 @@
 import tarfile
-
+import argparse
 from ops.data_ops import get_home
-
 
 def make_archive(output_filename, source_dirs):
     with tarfile.open(f"{output_filename}.tar.gz", "w:gz") as tar:
@@ -14,15 +13,22 @@ def make_archive(output_filename, source_dirs):
 
         print("Compression finished for", source_dirs)
 
-
 if __name__ == "__main__":
-    source_dirs = [
-        {"files": f"{get_home()}/peers", "dir": "peers"},
-        {"files": f"{get_home()}/blocks", "dir": "blocks"},
-        {"files": f"{get_home()}/index", "dir": "index"},
-        {"files": f"{get_home()}/peers", "dir": "peers"}
-    ]
+    parser = argparse.ArgumentParser(description="Backup specified folders into a tar.gz archive.")
+    parser.add_argument("--backup", nargs="*", help="List of folders to backup. If not provided, all folders will be backed up.")
+    parser.add_argument("--output", default="nado_archive", help="Output filename for the archive.")
 
-    output_filename = "nado_archive"
+    args = parser.parse_args()
 
-    make_archive(output_filename, source_dirs)
+    if args.backup:
+        source_dirs = [
+            {"files": f"{get_home()}/{folder}", "dir": folder} for folder in args.backup
+        ]
+    else:
+        source_dirs = [
+            {"files": f"{get_home()}/peers", "dir": "peers"},
+            {"files": f"{get_home()}/blocks", "dir": "blocks"},
+            {"files": f"{get_home()}/index", "dir": "index"},
+        ]
+
+    make_archive(args.output, source_dirs)
